687e5967457359793c065f048543593a3df1f624
rahulSutar001 <sutarrahul709@gmail.com>
2023-08-12 17:12:28 +0530

    New changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,19 @@
 <template>
     <div>
         <Header />
-        <div class="sm:flex sm:flex-warp bg-black">
-            <div class="grow w-full  mx-5 py-5">
+        <div class="sm:flex sm:flex-warp bg-black px-5">
+            <div class="grow py-5">
                 <span class="space-y-5">
                     <p class="text-5xl font-bold text-white">Get Verified Business Leads with AI Portal</p>
                     <p class="text-white"><b> Verified Business Lead = Name + Contact Details + Buying Intent of the
                             customer.</b> Now Imagine your sales team talking to only the highly qualified and exclusive
                         leads.</p>
                 </span>
-                <span class="space-x-4 space-y-5">
-                    <button class="btn bg-blue-400 border-none">Book a demo</button>
-                    <button class="btn border-none font-bold">Watch Introduction</button>
+                <span class="">
+                    <div class="flex  mt-5 ">
+                        <div class=""> <button class="btn bg-blue-400 border-none ">Book a demo</button></div>
+                        <div class=""> <button class="btn border-none font-bold  ml-5 ">Watch Introduction</button></div>
+                    </div>
                 </span>
                 <div class="flex space-x-2 mx-3 my-5">
                     <div>
@@ -29,18 +31,18 @@
                 </div>
             </div>
             <!-- div ends here  -->
-            <div class="grow w-full my-10 sm:ml-40 mx-5 ">
-                <div class="relative top-4 left-10 ">
+            <div class="mt-10 sm:ml-40 mx-5 ">
+                <div class="relative top-3 left-15 ">
                     <img src="../assets/leadg1.jpeg"
-                        class="sm:w-80 w-72 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ..."
+                        class="sm:w-[700px] sm:h-36 w-72 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ..."
                         alt="">
                 </div>
-                <div class="relative top-0 left-2">
+                <div class="relative top-0 sm:-left-10 left-5">
                     <img src="../assets/leadg3.jpeg"
-                        class="sm:w-80 w-64 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ..."
+                        class="sm:w-60 w-64 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ..."
                         alt="">
                 </div>
-                <div class="relative -top-10">
+                <div class="relative -top-8 sm:-top-5">
                     <img src="../assets/leadg2.jpeg"
                         class="sm:w-56 w-52 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ..."
                         alt="">
@@ -49,17 +51,118 @@
         </div>
 
         <!-- harness the happiness of  -->
-        <div>
-            <div class="text-center py-20 sm:px-24 px-5 bg-slate-300">
-                <p class="sm:text-4xl text-3xl">Harness the power of</p><br>
-                <p class="sm:text-5xl text-4xl font-bold">Artificial Intelligence to boost your sales</p><br>
-                <p class="sm:text-xl text-lg">To get you the right customer, your team can't work on 200+ Google Ad parameters
-                    24 x 7. No human can. But for artificial intelligence, it's a cake walk !!! AI works 24x7 tirelessly
-                    with ever increasing precision to get you Verified Business Lead.</p>
-                    <br>
-                    <p class="sm:text-lg text-sm">Let AI help you identify the right plan for you</p>
-                    <br>
-                    <button class="btn btn-primary text-white font-semibold">Start AI guided plan</button>
+
+        <div class="text-center py-20 sm:px-24 px-5 bg-slate-100">
+            <p class="sm:text-4xl text-3xl">Harness the power of</p><br>
+            <p class="sm:text-5xl text-4xl font-bold">Artificial Intelligence to boost your sales</p><br>
+            <p class="sm:text-xl text-lg">To get you the right customer, your team can't work on 200+ Google Ad parameters
+                24 x 7. No human can. But for artificial intelligence, it's a cake walk !!! AI works 24x7 tirelessly
+                with ever increasing precision to get you Verified Business Lead.</p>
+            <br>
+            <p class="sm:text-lg text-sm">Let AI help you identify the right plan for you</p>
+            <br>
+            <button class="btn btn-primary text-white font-semibold">Start AI guided plan</button>
+        </div>
+
+
+        <div class="text-center py-20 px-5">
+
+            <h1 class="text-5xl my-5"> <span class="font-extrabold">We capture</span> <span class="font-thin">200+ Google
+                    Ads Paramenters</span> <br>
+                <span class="font-extrabold">for your business</span>
+            </h1>
+            <p class="mt-5 mb-16 text-lg">Do you still follow the old school method of manual marketing? You should
+                definitely see a demo <br> of AI portal to see what you are missing out! Bring in the "Inaiways AI Portal"
+                for AI driven <br> marketing for your business. <br>
+                AI Portal is the top selling product of our Business Productivity Suite: "The Business Studio".
+            </p>
+
+            <div class="bg-gray-200 py-10">
+
+                <iframe class="mx-auto w-72 sm:w-3/4  sm:h-96 h-60 rounded-lg"
+                    src="https://www.youtube.com/embed/h0kZF5vlWWQ" title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen></iframe>
+
+            </div>
+        </div>
+
+        <div class="bg-black py-10 my-5 text-center text-white">
+            <p class="text-yellow-200"><small>TOP REASONS TO JOIN US</small></p>
+            <p class="sm:text-5xl md:text-2xl text-xl font-extrabold">How does AI Driven Lead <br>
+                Generation Benefit My <br>
+                Business?
+            </p>
+            <p class="text-xl my-5 text-gray-400"><small>Book a sales demo today and get an early bird discount!</small></p>
+            <div class="grid md:grid-cols-3 sm:grid-cols-1 grid-cols-1 px-10 sm:px-5 gap-3 ">
+                <div class="text-center">
+                    <span>1</span>
+                    <p class="font-bold text-xl">Get so far undiscovered market insights!</p>
+                    <p class="text-slate-400">Get ahead of your competition by harnessing the power of artificial
+                        intelligence in Google ad keyword bidding</p>
+                </div>
+                <div class=" text-center ">
+                    <span>2</span>
+                    <p class="font-bold text-xl">Higher chances of lead conversion!</p>
+                    <p class="text-slate-400">Avail the service in your existing marketing budget, no extra cost and get
+                        top-quality leads</p>
+                </div>
+                <div class=" text-center ">
+                    <span>3</span>
+                    <p class="font-bold text-xl">Insights on ‘buying intent’ of every single lead!</p>
+                    <p class="text-slate-400">We capture Google search-typed keywords by the users and create unlimited
+                        landing pages on the fly; based on the keywords, along with Q&A</p>
+                </div>
+                <div class=" text-center ">
+                    <span>4</span>
+                    <p class="font-bold text-xl">Insights on ‘buying intent’ of every single lead!</p>
+                    <p class="text-slate-400">We capture Google search-typed keywords by the users and create unlimited
+                        landing pages on the fly; based on the keywords, along with Q&A</p>
+                </div>
+                <div class=" text-center ">
+                    <span>5</span>
+                    <p class="font-bold text-xl">Insights on ‘buying intent’ of every single lead!</p>
+                    <p class="text-slate-400">We capture Google search-typed keywords by the users and create unlimited
+                        landing pages on the fly; based on the keywords, along with Q&A</p>
+                </div>
+            </div>
+            <div class="my-28 space-y-3 px-5">
+                <p class="font-bold text-lg">Want to reduce your expenses for advertising straight by 90%?</p>
+                <p class="text-lg text-slate-400">Get in touch with us and increase your Top-Line and Bottom-Line with
+                    excellent Lead conversion</p>
+                <button class="btn bg-blue-600 text-white border-none">Book a demo & understand how to get more leads with
+                    AI</button>
+            </div>
+        </div>
+
+        <div class="px-5 sm:grid sm:grid-cols-2  space-x-10">
+            <div class="">
+                <p class="text-blue-500 font-bold text-sm">SETTING A NEW STANDARD</p>
+                <p class="sm:text-5xl text-3xl my-4">AI portal <br>
+                    <span class="font-extrabold">trusted by our <br>
+                        delighted <br>
+                        clients
+                    </span>
+                </p>
+                <div class="flex flex-wrap w-full space-x-0 sm:space-x-4 space-y-4 sm:space-y-0  my-4">
+                    <div class="grow w-full">
+                        <span class="flex sm:text-left text-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-4 mt-3 mr-3 h-4 text-blue-500">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                            </svg>
+                            <p class="mt-2 "><b>Read Customer Stories</b></p>
+                        </span>
+                    </div>
+                    <div class="grow w-full"><button class="btn bg-blue-600 text-white border-none rounded-sm">Get me more customer</button></div>
+                </div>
+            </div>
+            <div class=" ">
+                <div class="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 ">
+               <div><img src="../assets/tata.jpg" class="w-20" alt=""></div>
+               <div><img src="../assets/DPU-color-Logo.png" class="w-20" alt=""></div>
+               <div><img src="../assets/sra.png" class="w-20" alt=""></div>
+               </div>
             </div>
         </div>
 
